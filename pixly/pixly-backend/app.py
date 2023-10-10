cfa53b90adea336bb498894c785ccddd542e7438
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-10-10 15:25:45 -0700

    Set up presigned URL from s3

--- pixly/pixly-backend/app.py
+++ pixly/pixly-backend/app.py
@@ -2,18 +2,17 @@ from flask import Flask
 from dotenv import load_dotenv
 import boto3
 import os
+import requests
 
-
+EXPIRES_IN_6_DAYS = 518400
 SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 PUBLIC_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
-
 
 app = Flask(__name__)
 
 load_dotenv()
 
-# s3 = boto3.client('s3')
-
+#S3 photo storage
 s3 = boto3.client(
     "s3",
     "us-east-2",
@@ -21,12 +20,34 @@ s3 = boto3.client(
     aws_secret_access_key=SECRET_ACCESS_KEY,
 )
 
+our_bucket = 'r33-grace-steve-pixly'
+test_pic_key = 'test-jpg2'
 
+# gets all buckets
 response = s3.list_buckets()
-
 for bucket in response['Buckets']:
     print(f'  {bucket["Name"]}')
 
-result = s3.upload_file('./as-above-so-below.jpg', 'r33-grace-steve-pixly', 'test-jpg')
+#uploads an image to a known bucket
+result = s3.upload_file(
+    './as-above-so-below.jpg',
+    our_bucket,
+    test_pic_key,
+    ExtraArgs= {'ContentType': 'image/jpeg'})
 
 print('result is: ', result)
+
+#get AWS image URL
+presigned_url = s3.generate_presigned_url(
+    ClientMethod="get_object",
+    Params={
+        "Bucket": our_bucket,
+        "Key": test_pic_key
+    },
+    ExpiresIn=EXPIRES_IN_6_DAYS
+)
+print(presigned_url, "presignedURL")
+
+# if presigned_url is not None:
+#     response = requests.get(presigned_url)
+#     print("photo url: ", response)
