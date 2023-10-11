0b59c09e16a9dd88580229151f2de25920a92ead
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-10-11 10:36:16 -0700

    DB table completed and started post route with upload to s3

--- pixly/pixly-backend/app.py
+++ pixly/pixly-backend/app.py
@@ -1,98 +1,60 @@
-from flask import Flask
-from dotenv import load_dotenv
+from flask import Flask, request
 import boto3
 import os
 from PIL import Image, ExifTags
 from PIL.ExifTags import TAGS
-#from models import db, connect_db
-from datetime import date
-from dateutil.parser import parse
+from models import db, connect_db
+
 
 #import requests
 
-EXPIRES_IN_6_DAYS = 518400
-SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-PUBLIC_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
-
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-#     "DATABASE_URL", 'postgresql:///pixly')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///pixly')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
-# connect_db(app)
-
-load_dotenv()
-
-#S3 photo storage
-s3 = boto3.client(
-    "s3",
-    "us-east-2",
-    aws_access_key_id=PUBLIC_ACCESS_KEY,
-    aws_secret_access_key=SECRET_ACCESS_KEY,
-)
-
-our_bucket = 'r33-grace-steve-pixly'
-test_pic_key = 'test-jpg2'
-
-# gets all buckets
-response = s3.list_buckets()
-for bucket in response['Buckets']:
-    print(f'  {bucket["Name"]}')
-
-#uploads an image to a known bucket
-result = s3.upload_file(
-    './as-above-so-below.jpg',
-    our_bucket,
-    test_pic_key,
-    ExtraArgs= {'ContentType': 'image/jpeg'})
-
-print('result is: ', result)
-
-#get AWS image URL
-presigned_url = s3.generate_presigned_url(
-    ClientMethod="get_object",
-    Params={
-        "Bucket": our_bucket,
-        "Key": test_pic_key
-    },
-    ExpiresIn=EXPIRES_IN_6_DAYS
-)
-print(presigned_url, "presignedURL")
+connect_db(app)
 
 
-#get metadata from an image
-# helper functions
-    # function to convert date and time string to db compatible date/time str
-with Image.open("./canon_hdr_YES.jpg") as test_image:
-    sample_exif = test_image.getexif()
-    for tag_id in sample_exif:
-        tag_name = TAGS.get(tag_id, tag_id)
-        value = sample_exif.get(tag_id)
-        #print(f'{tag_name},{value}')
+#routes:
+#add a photo
+@app.post('/add')
+def add_photo():
+    """Adds a photo"""
 
-# with Image.open("./DSCN0021.jpg") as test_image2:
-#     sample_exif = test_image2.getexif()
-#     for tag_id in sample_exif:
-#         tag_name = TAGS.get(tag_id, tag_id)
-#         value = sample_exif.get(tag_id)
-#         print(f'{tag_name},{value},{type(value)}')
-#         if tag_name == 'DateTime':
-#             print('converted date: ', parse(value, yearfirst=True, ignoretz=True))
+    # formdata
+    photo = request.files["url"]
+    # massage this data to go into s3
 
-test_image3 = Image.open("./DSCN0021.jpg")
-exif = test_image3.getexif()
-print(exif, "EXIF OBJ")
-gps_ifd = exif.get_ifd(ExifTags.IFD.GPSInfo)
-print(gps_ifd, "GPS DATA")
+    # create a key for s3
+    # get the url back from s3
+    # extract metadata from photo
+    # massage the data to go into db
 
-#GPS_data = sample_exif.get_ifd(ExifTags.TAGS[306])
-#print("EXIF data: ", sample_exif)
-#print("GPS_data is: ", GPS_data)
+    # Photo(url, gps_info, camera_make, camera_model, image_description, date)
+    #sumbit to database
 
-# if presigned_url is not None:
-#     response = requests.get(presigned_url)
-#     print("photo url: ", response)
+    # json back to front end- {message: success to upload photo, statuscode: 201}
+
+#get all photos
+@app.get('/photos')
+def get_all_photos():
+    """Get all photos."""
+    #query the database for all photos
+    # json with list of photos
+
+
+@app.get('/photos/<in:id>')
+def get_photo(id):
+    """Get a photo."""
+    #query database for photo by id
+    #json with info on photo
+
+
+
+
+
 
 #EXIF fields to start off: GPSInfo, Make, Model, ImageDescription, DateTime, Software?
 
--- pixly/pixly-backend/models.py
+++ pixly/pixly-backend/models.py
@@ -2,28 +2,50 @@ from flask_sqlalchemy import SQLAlchemy
 #from app import app
 db = SQLAlchemy()
 
-# def connect_db(app):
-#     """Connect to database."""
-#     app.app_context().push()
-#     db.app = app
-#     db.init_app(app)
+def connect_db(app):
+    """Connect to database."""
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
 
-# class Photo(db.Model){
-#     __tablename__ = "photos"
+class Photo(db.Model):
+    """Photos in the system."""
 
-#     id = db.Column(
-#         db.Integer,
-#         primary_key=True,
-#         autoincrement=True
-#     )
+    __tablename__ = "photos"
 
-#     url = db.Column(
-#         db.String,
-#         nullable=False
-#     )
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
 
-#     gps_info = db.Column(
-#         db.Integer
-#     )
-# }
+    url = db.Column(
+        db.String,
+        nullable=False,
+        unique=True
+    )
 
+    gps_info = db.Column(
+        db.Integer,
+        nullable=True
+    )
+
+    camera_model= db.Column(
+        db.String,
+        nullable=True
+    )
+
+    camera_make= db.Column(
+        db.String,
+        nullable=True
+    )
+
+    image_description = db.Column(
+        db.String,
+        nullable=True
+    )
+
+    date = db.Column(
+        db.String,
+        nullable=True
+    )
