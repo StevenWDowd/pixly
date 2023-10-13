5c807ce8b423051218edf0fb4fe8f02ac2e2aa71
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-10-12 17:57:34 -0700

    added black and white functionality'

--- pixly/pixly-backend/utils.py
+++ pixly/pixly-backend/utils.py
@@ -1,4 +1,3 @@
-
 import boto3
 import os
 from dotenv import load_dotenv
@@ -6,7 +5,7 @@ from uuid import uuid4
 from PIL import Image, ExifTags
 from PIL.ExifTags import TAGS
 from tempfile import NamedTemporaryFile
-from io import IOBase
+
 
 load_dotenv()
 
@@ -15,14 +14,13 @@ PIXLEY_BUCKET = 'r33-grace-steve-pixly'
 SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 PUBLIC_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
 
-
-#S3 photo storage
 s3 = boto3.client(
     "s3",
     "us-east-2",
     aws_access_key_id=PUBLIC_ACCESS_KEY,
     aws_secret_access_key=SECRET_ACCESS_KEY,
 )
+
 
 def create_presigned_url(key):
     """Creates presigned URL for object in s3 bucket"""
@@ -31,22 +29,15 @@ def create_presigned_url(key):
         Params={
             "Bucket": PIXLEY_BUCKET,
             "Key": key
-    },
-    ExpiresIn=EXPIRES_IN_6_DAYS
+        },
+        ExpiresIn=EXPIRES_IN_6_DAYS
     )
     return presigned_url
+
 
 def upload_to_s3(image_file):
     """Uploads an image to S3 storage"""
     new_key = str(uuid4())
-    # temp = tempfile.NamedTemporaryFile()
-    # with open(image_file.filename, "rb") as f:
-    #     jpg_data = f.read
-    #     open(temp, "rb")
-    #     temp.write(jpg_data)
-    #     temp.close
-
-    # with open(image_file, "rb") as readable_file:
 
     s3.upload_fileobj(
         image_file,
@@ -55,6 +46,7 @@ def upload_to_s3(image_file):
         ExtraArgs={'ContentType': 'image/jpeg'})
     print("uploaded")
     return new_key
+
 
 def get_exif_data(image_file):
     """Retrieves EXIF data from an image file, returns a dict. """
@@ -66,7 +58,6 @@ def get_exif_data(image_file):
         "date": None
     }
 
-    #TODO: use load or new with image_file?
     with Image.open(image_file) as img:
         exif_data = img.getexif()
         for tag_id in exif_data:
@@ -87,90 +78,20 @@ def get_exif_data(image_file):
 
     return image_exif_dict
 
+
 def black_white_photo(key):
-    temp = NamedTemporaryFile(delete=False)
-    #s3.download_file(PIXLEY_BUCKET, key, temp.name)
-    s3.download_fileobj(PIXLEY_BUCKET, key, temp)
-    temp.seek(0)
-    #temp has our photo data
-    print("key is ", key)
-    #print("temp is: ", temp)
-    #print("tempname is: ", temp.name)
-    #print("cursor position: ", temp.tell())
-    #temp.close()
-    print("temp closed?", temp.closed)
-    #print("temp after closing is: ", temp)
-    #print(temp.readlines())
+    with NamedTemporaryFile(suffix=".jpg", delete=False) as temp:
+        s3.download_file(PIXLEY_BUCKET, key, temp.name)
 
-    with Image.open(temp) as t:
-        print("entered Image with block")
-        t.convert(mode="L")
-        print(t.format)
-        t.seek(0)
+        print("key is ", key)
 
-    # s3.upload_fileobj(
-    #     temp,
-    #     PIXLEY_BUCKET,
-    #     key,
-    #     ExtraArgs={'ContentType': 'image/jpeg'})
+        with Image.open(temp.name) as t:
+            print("entered Image with block")
+            gray_scale = t.convert("L")
+            gray_scale.save(temp.name)
+            print(t.format, "this is format")
+            s3.upload_file(
+                temp.name,
+                PIXLEY_BUCKET,
+                key)
 
-
-
-
-
-# test_pic_key = 'test-jpg2'
-
-# # gets all buckets
-# response = s3.list_buckets()
-# for bucket in response['Buckets']:
-#     print(f'  {bucket["Name"]}')
-
-# #uploads an image to a known bucket
-# result = s3.upload_file(
-#     './as-above-so-below.jpg',
-#     our_bucket,
-#     test_pic_key,
-#     ExtraArgs= {'ContentType': 'image/jpeg'})
-
-# print('result is: ', result)
-
-# #get AWS image URL
-# presigned_url = s3.generate_presigned_url(
-#     ClientMethod="get_object",
-#     Params={
-#         "Bucket": our_bucket,
-#         "Key": test_pic_key
-#     },
-#     ExpiresIn=EXPIRES_IN_6_DAYS
-# )
-# print(presigned_url, "presignedURL")
-
-
-# #get metadata from an image
-# # helper functions
-#     # function to convert date and time string to db compatible date/time str
-# with Image.open("./canon_hdr_YES.jpg") as test_image:
-#     sample_exif = test_image.getexif()
-#     for tag_id in sample_exif:
-#         tag_name = TAGS.get(tag_id, tag_id)
-#         value = sample_exif.get(tag_id)
-#         #print(f'{tag_name},{value}')
-
-# # with Image.open("./DSCN0021.jpg") as test_image2:
-# #     sample_exif = test_image2.getexif()
-# #     for tag_id in sample_exif:
-# #         tag_name = TAGS.get(tag_id, tag_id)
-# #         value = sample_exif.get(tag_id)
-# #         print(f'{tag_name},{value},{type(value)}')
-# #         if tag_name == 'DateTime':
-# #             print('converted date: ', parse(value, yearfirst=True, ignoretz=True))
-
-# test_image3 = Image.open("./DSCN0021.jpg")
-# exif = test_image3.getexif()
-# print(exif, "EXIF OBJ")
-# gps_ifd = exif.get_ifd(ExifTags.IFD.GPSInfo)
-# print(gps_ifd, "GPS DATA")
-
-# #GPS_data = sample_exif.get_ifd(ExifTags.TAGS[306])
-# #print("EXIF data: ", sample_exif)
-# #print("GPS_data is: ", GPS_data)
